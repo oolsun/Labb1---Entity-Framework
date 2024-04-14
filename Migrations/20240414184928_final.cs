089940c372399ed53f4662df24a04c127340f8fa
Olov <olovolsson@live.se>
2024-04-14 20:49:41 +0200

    .

---
+++ Migrations/20240414184928_final.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Labb1.Migrations
+{
+    /// <inheritdoc />
+    public partial class final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
